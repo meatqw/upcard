33c44130cf4e9a93090fce86e5aaf71cc699b045
meatqw <lexerdxx@gmail.com>
2023-05-01 15:10:18 +0700

    v0.7

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-// export const API_DOMAIN = 'http://upcard.online'
-export const API_DOMAIN = 'http://127.0.0.1:8000'
+export const API_DOMAIN = 'http://upcard.online'
+// export const API_DOMAIN = 'http://127.0.0.1:8000'
 
 
 
